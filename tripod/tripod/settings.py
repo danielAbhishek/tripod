26f932dbbb9960264b06aef1c70b455cc046ef7e
daniel abhishek <daniel.abhishek002@gmail.com>
2022-05-25 14:18:44 +0530

    setting postgres and completing setup

--- tripod/tripod/settings.py
+++ tripod/tripod/settings.py
@@ -109,6 +109,7 @@ DATABASES = {
     'default': {
         #  'ENGINE': 'django.db.backends.sqlite3',
         #  'NAME': BASE_DIR / 'db.sqlite3',
+
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tripod',
         'USER': 'tripod',
